a45ba84f8c2162ac6b8a7694284982b44f190e93
CPZ <13316865662@163.com>
2024-05-22 20:44:15 +0800

    添加了首页和登录页

--- Readme.md
+++ Readme.md
@@ -1,9 +1,12 @@
 目前结构
 --jnueca
+    --venv(环境)
     --sattic(静态文件夹)
+        --js(js文件夹)
         --css(css文件夹)
         --img(图片文件夹)
     --templates(flask模板文件夹)
         -index.html
+        -login.html
     -app.py(程序主页)
     -Readme.md
--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask,render_template
-from flask import url_for
+from flask import url_for,request,redirect,flash
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost' # MySQL主机地址
+app.config['MYSQL_USER'] = 'root' # MySQL用户名
+app.config['MYSQL_PASSWORD'] = 'root' # MySQL密码
+app.config['MYSQL_DB'] = 'jnueca' # MySQL数据库名
+
+db = MySQL(app)#连接数据库并创建对象
+
+@app.route('/index')
 @app.route('/')
 def index():
     return render_template("index.html")
 @app.route('/game')
 def game():
     return render_template('game.js')
+@app.route('/login',methods=['GET','POST'])
+def login():
+        if request.method == 'POST':  # 判断是否是 POST 请求
+            # 获取表单数据
+            phone = request.form.get('phone') 
+            password = request.form.get('password')
+            # 验证数据
+            if not phone or password==False:
+                flash('Invalid input.')  # 显示错误提示
+                return redirect(url_for('login'))  # 重定向回login页
+            # 保存表单数据到数据库
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO User (phone, password) VALUES (%s, %s)", (phone, password))
+            mysql.connection.commit()
+            return redirect(url_for('index'))
+        flash('登录成功')
+        return render_template('login.html')
 app.run()
